7ed6efed25f530f59a7d131c78801b41035351ab
Oliver Schmid <oliver.schmid@epfl.ch>
2018-03-12 10:21:04 +0100

    update to v0.1.3

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ reqs = [str(ir.req) for ir in install_reqs]
 
 setup(
     name='pyxus',
-    version='0.1.2',
+    version='0.1.3',
     packages=['pyxus', 'pyxus.resources', 'pyxus.utils'],
     install_requires = reqs
 )
